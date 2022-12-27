460f988fa12ef77de71b787247e4427f77947988
Carolina Araújo <carolinaa.araujo00@gmail.com>
2022-12-27 18:53:51 +0000

    updates to office: more immersive
    - remove mouse pointer (setCursorHidden and as relative to the window, where the mouse is placed in the middle of the window and relative movements are reported)
    - updated office scale and the torch placement
    - changed movement and camera speed

--- level_two/office.py
+++ level_two/office.py
@@ -1,7 +1,7 @@
 import math
 
 from direct.showbase.ShowBase import ShowBase
-from direct.interval.LerpInterval import LerpHprInterval
+from pandac.PandaModules import WindowProperties
 from panda3d.core import loadPrcFileData
 from panda3d.core import *
 from common import *
@@ -27,7 +27,7 @@ class ociffer(ShowBase):
         self.init_movement()
 
         setup_black_ambient_light(self.render)
-        # setup_point_light(self.render, (10, 10, 10))
+
         self.set_background_color(0, 0, 0, 1)
 
         self.sound_player = SoundPlayerTwo(self)
@@ -37,51 +37,60 @@ class ociffer(ShowBase):
 
         self.props = self.win.getProperties()
         self.screensizeX = self.props.getXSize() / 2
+
+        self.props = WindowProperties()
+        self.props.setCursorHidden(True)
+        self.props.setMouseMode(WindowProperties.M_relative)
+        self.win.requestProperties(self.props)
         
-        self.cam.setPos(6, 7, 5) # X = left & right, Y = zoom, Z = Up & down.
+        self.cam.setPos(6, 7, 6) # X = left & right, Y = zoom, Z = Up & down.
         self.cam.setHpr(140, -20, 0) # Heading, pitch, roll.
 
         # load models
         self.setup_office()
         self.setup_office_room()
-        self.setup_hands()
+        self.setup_hand()
         self.setup_torch()
         self.setup_cockroach()
         self.setup_printer()
+        self.setup_ceiling_lights()
 
         # Play Sounds
         self.sound_player.play_sounds()
-        self.setup_ceiling_lights()
-        
         self.sound_player.play_lights_on()
+
 
     def setup_office(self):
         self.office_model = self.loader.loadModel(office_model_path)
-        self.office_model.setScale(0.5,0.5,0.5)
+        self.office_model.setScale(0.8)
         self.office_model.reparentTo(self.render)
+
 
     def setup_office_room(self):
         self.office_room_model = self.loader.loadModel(office_room_model_path)
-        self.office_room_model.setScale(0.7,0.7,0.7)
+        self.office_room_model.setScale(0.7)
         self.office_room_model.reparentTo(self.render)
 
-    def setup_hands(self):
-        self.hands = self.loader.loadModel(hand_model_path)
-        self.hands.setScale(self.cam, 0.2)
-        self.hands.reparentTo(self.render)
+
+    def setup_hand(self):
+        self.hand = self.loader.loadModel(hand_model_path)
+        self.hand.setScale(self.cam, 0.2)
+        self.hand.reparentTo(self.render)
+
 
     def setup_torch(self):
         self.sphere = self.loader.loadModel(sphere_model_path)
-        self.sphere.setScale(0.05)
-        self.sphere.setPos(-1.7, -0.68, 2.25)
+        self.sphere.setScale(0.1)
+        self.sphere.setPos(-3, -1, 3.7)
         self.sphere.reparentTo(self.render)
 
         self.torch = self.loader.loadModel(torch_model_path)
-        self.torch.setPos(-1.7, -0.68, 2)
-        self.torch.setScale(0.5)
+        self.torch.setPos(-3, -1, 3.25)
+        self.torch.setScale(0.9)
         self.torch.reparentTo(self.render)
 
         setup_torch_spotlight(self.render, self.sphere, (-1.5, -0.21, 2))
+
 
     def setup_cockroach(self):
         self.cockroach = Cockroach(self.office_model, Vec3(-4.87, 0.43, 3.4))
@@ -97,28 +106,28 @@ class ociffer(ShowBase):
 
     def setup_ceiling_lights(self):
         self.c_lamp = self.loader.loadModel(ceiling_lamp_model_path)
-        self.c_lamp.setPos(-8, -8, 4)
-        self.c_lamp.setScale(2)
+        self.c_lamp.setPos(-8, -8, 2)
+        self.c_lamp.setScale(4)
         self.c_lamp.reparentTo(self.render)
+
 
     # Called every frame
     def update(self, task):
-
         # globalClock is, naturally, a panda3d global, despite what the IDE might say
         self.dt = globalClock.getDt()
 
         self.check_movement(task)
         self.mousePosition(task)
 
-        self.hands.setPos(self.cam, (0.8, 5, -1.3))
-        self.hands.setHpr(self.cam, (200, -45, 0))
-        self.hands.setScale(self.cam, 0.2)
+        self.hand.setPos(self.cam, (0.8, 5, -1.3))
+        self.hand.setHpr(self.cam, (200, -45, 0))
+        self.hand.setScale(self.cam, 0.2)
 
         return task.cont
 
 
     def init_movement(self):
-        self.speed = 4
+        self.speed = 6
         self.angle = 0
 
         self.accept("a", updateKeyMap, ["left", True])
@@ -144,8 +153,6 @@ class ociffer(ShowBase):
     def check_movement(self, task):
         cam_pos = self.cam.getPos()
 
-        # Converts camera rotation to a percentage of direction towards X and Y, then applies it
-        # Source: https://stackoverflow.com/questions/4550315/python-convert-degrees-to-change-in-x-and-change-in-y
         speed = self.speed * self.dt
         angle = math.radians(self.cam.getH())    # Remember to convert to radians!
         
@@ -181,28 +188,24 @@ class ociffer(ShowBase):
         
         # get the relative mouse position, 
         # its always between 1 and -1
-        mpos = self.mouseWatcherNode.getMouse()
+        mouse_pos = self.mouseWatcherNode.getMouse()
 
         cam_rotation_p = self.cam.getP()
         cam_rotation_h = self.cam.getH()
         
-        if mpos.getX() > 0.2: # and self.cam.getH() > -15:
-            cam_rotation_h -= self.speed*10 * self.dt
 
-        elif mpos.getX() < -0.2: # and self.cam.getH() < 15:
-            cam_rotation_h += self.speed*10 * self.dt
+        if mouse_pos.getX() > 0.2: # and self.cam.getH() > -15:
+            cam_rotation_h -= self.speed * 8 * self.dt
 
-        if mpos.getY() > 0.2 and self.cam.getP() < 10:
-            cam_rotation_p += self.speed*10 * self.dt
+        elif mouse_pos.getX() < -0.2: # and self.cam.getH() < 15:
+            cam_rotation_h += self.speed * 8 * self.dt
 
-        elif mpos.getY() < -0.2 and self.cam.getP() > -20:
-            cam_rotation_p -= self.speed*10 * self.dt
+        if mouse_pos.getY() > 0.2 and self.cam.getP() < 15:
+            cam_rotation_p += self.speed * 8 * self.dt
+
+        elif mouse_pos.getY() < -0.2 and self.cam.getP() > -20:
+            cam_rotation_p -= self.speed * 8 * self.dt
 
         self.cam.setHpr((cam_rotation_h, cam_rotation_p, 0))
 
         return task.cont
-
-
-if __name__ == "__main__":
-    game = ociffer()
-    game.run()
