88333d70d0228410b68190cf5801700d87d8e378
Franciscorp <franciscorp1994@hotmail.com>
2022-12-28 20:35:44 +0000

    Give me my phone animation

--- level_two/office.py
+++ level_two/office.py
@@ -243,10 +243,20 @@ class ociffer():
 
     def setup_telephone(self):
         self.telephone_base = self.base.loader.loadModel(telephone_base_model_path)
-        self.telephone_base.setPos(1.05, -3.57, -3.84)
-        # self.telephone_base.setHpr(180,0,0)
-        # self.telephone_base.setScale(0.25)
+        self.telephone_base.setPos(1.05, -2.6, 3.1)
+        self.telephone_base.setScale(0.75)
         self.telephone_base.reparentTo(self.base.render)
+
+        # self.telephone_ring = self.base.loader.loadModel(telephone_ring_model_path)
+        self.telephone_ring = Actor(telephone_ring_model_path, {"ring": telephone_ring_model_path})
+        # self.telephone_ring = Actor(telephone_ring_model_path)
+        self.telephone_ring.loop("ring")
+        self.telephone_ring.setPlayRate(1, 'ring')
+        self.telephone_ring.setScale(0.75)
+        self.telephone_ring.setPos(1.05, -2.6, 3.1)
+        # self.telephone_ring.setPos(0, 0, 3.1)
+        self.telephone_ring.reparentTo(self.base.render)
+
 
 
     # Called every frame
